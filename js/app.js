b16a67b04db033ec8bd5137f5f382d082f5ddb58
zahangir710 <zahangir710@gmail.com>
2021-09-13 02:13:16 +0200

    bug fix

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 const loadProducts = () => {
   const url = `https://fakestoreapi.com/products`;
+  // const url=('../data.json');
   fetch(url)
     .then((response) => response.json())
     .then((data) => showProducts(data));
